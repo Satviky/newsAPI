d596d12ad89ceca01cc340eff6efe010ac4111f0
Satviky <126392608+Satviky@users.noreply.github.com>
2025-02-18 18:51:20 +0530

    Update news.py

--- news.py
+++ news.py
@@ -1,11 +1,6 @@
-# importing requests package
 import requests	
 
 def NewsFromBBC():
-	
-	# BBC news api
-	# following query parameters are used
-	# source, sortBy and apiKey
 	query_params = {
 	"source": "bbc-news",
 	"sortBy": "top",
@@ -13,11 +8,8 @@ def NewsFromBBC():
 	}
 	main_url = " https://newsapi.org/v1/articles"
 
-	# fetching data in json format
 	res = requests.get(main_url, params=query_params)
 	open_bbc_page = res.json()
-
-	# getting all articles in a string article
 	article = open_bbc_page["articles"]
 
 	# empty list which will
@@ -29,15 +21,12 @@ def NewsFromBBC():
 		
 	for i in range(len(results)):
 		
-		# printing all trending news
 		print(i + 1, results[i])
 
-	#to read the news out loud for us
 	from win32com.client import Dispatch
 	speak = Dispatch("SAPI.Spvoice")
 	speak.Speak(results)				
 
-# Driver Code
 if __name__ == '__main__':
 	
 	# function call
